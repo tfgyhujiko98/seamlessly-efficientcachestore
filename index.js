db35e39c1bffdec24ee9607337547b05c80e8b63
tfgyhujiko98 <gs1alan.bit@hotmail.com>
2024-05-13 14:13:49 +0700

    start

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const bubbleSort = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-      }
-    }
-  }
-  return arr;
-};
+const initializeArrayWithValues = (n, value = 0) => Array(n).fill(value);
